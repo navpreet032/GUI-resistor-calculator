d8233fc1b469ad8947b07b9fa7bd2cdec84d0a35
navpreet032 <55250212+navpreet032@users.noreply.github.com>
2020-12-29 19:11:55 +0530

    Delete cMain.h

--- cMain.h
+++
@@ -1,9 +0,0 @@
-#pragma once //                      THIS CLASS IS **GUI**
-#include"wx/wx.h"
-class cMain :public wxFrame //*wxframe* its the base for "gui"
-{
-public :
-	cMain();
-	~cMain();
-};
-
