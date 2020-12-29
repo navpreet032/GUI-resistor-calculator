96015e85e6635cd8dcf29bda0833fde07205dc39
navpreet032 <55250212+navpreet032@users.noreply.github.com>
2020-12-29 19:10:50 +0530

    Delete cMain.cpp

--- cMain.cpp
+++
@@ -1,9 +0,0 @@
-#include "cMain.h"
-
-cMain::cMain():wxFrame(nullptr,wxID_ANY,"GUI RESIS CAL")// wxframe doest have parent so *nullptr*,all windows in Wxwidigt req an ID so *wxID_ANY* to give it something
-{                                                       // "GUI RESIS CAL" is the title of window
-}
-cMain::~cMain()
-{
-
-}
